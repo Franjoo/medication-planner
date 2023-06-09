0264931b15cb73802861aa63dbc0e62528b4d4be
Franz Benthin <franz@hexdox.com>
2023-06-09 08:35:34 +0200

    ✨ add scrollbar for weekly schedule

---
+++ src/components/atoms/Scrollbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { lerp } from "../../utils";
+import useMeasure from "react-use-measure";
+
+interface Props {
+  progress: number;
+  size: number;
+}
+
+const Scrollbar = ({ progress, size }: Props) => {
+  const [ref, bounds] = useMeasure();
+
+  const width = lerp(bounds.width * 0.1, bounds.width * 0.95, 1 - size);
+  const translateX = (bounds.width - width) * progress;
+
+  return (
+    <div className="mb-9 w-full" ref={ref}>
+      <div
+        className="h-3 bg-black transition-transform"
+        style={{
+          width: width + "px",
+          transform: `translateX(${translateX}px)`,
+        }}
+      />
+      <div className="h-[1px] w-full bg-black" />
+    </div>
+  );
+};
+
+export default Scrollbar;
--- src/components/container/WeeklyScheduleContainer.tsx
+++ src/components/container/WeeklyScheduleContainer.tsx
@@ -2,19 +2,31 @@ import WeeklyScheduleTableGroup from "../groups/WeeklyScheduleTableGroup";
 import { useStore } from "../../hooks/useStores";
 import { observer } from "mobx-react";
 import { Day } from "../../models";
+import Scrollbar from "../atoms/Scrollbar";
+import { clamp } from "../../utils";
 
 const WeeklyScheduleContainer = observer(() => {
   const { schedule } = useStore();
 
   const onAddEntryClick = (day: Day) => schedule.addTimeEntry(day.date);
   const onRemoveEntryClick = (day: Day) => schedule.removeTimeEntry(day.date);
+  const scrollbarSize = clamp(schedule.days.length / 100, 0, 1);
+  const scrollbarProgress = schedule.scrollProgress;
+  const scrollbarEnabled = schedule.canGoBackward || schedule.canGoForward;
 
   return (
-    <WeeklyScheduleTableGroup
-      days={schedule.displayDays}
-      onAddEntryClick={onAddEntryClick}
-      onRemoveEntryClick={onRemoveEntryClick}
-    />
+    <>
+      <WeeklyScheduleTableGroup
+        days={schedule.displayDays}
+        templateDays={schedule.templateDays}
+        showTemplates={schedule.showTemplates}
+        onAddEntryClick={onAddEntryClick}
+        onRemoveEntryClick={onRemoveEntryClick}
+      />
+      {scrollbarEnabled && (
+        <Scrollbar progress={scrollbarProgress} size={scrollbarSize} />
+      )}
+    </>
   );
 });
 
